2d91250218b68071738e6c3fa2f8454be3b3eab5
SamMcSam <sam@sam.sam>
2020-11-09 23:16:29 +0100

    style

--- assets/Home.js
+++ assets/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <Link className={"navbar-brand"} to={"/"}> Symfony React Project </Link>
+                    <Link className={"navbar-brand"} to={"/"}> Mauro API dashboard </Link>
                     <div className="collapse navbar-collapse" id="navbarText">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
